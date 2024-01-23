73c99b41eed022ea0fcba9769aac7e4d0bad0dd5
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-01-24 03:39:44 +0700

    afafa

--- src/js/user/config/config.js
+++ src/js/user/config/config.js
@@ -1,17 +1,14 @@
-import {
-    setCookieWithExpireHour
-} from 'https://jscroot.github.io/cookie/croot.js';
+import {setCookieWithExpireHour} from 'https://jscroot.github.io/cookie/croot.js';
 
 let userToken;
 
-//token api
 export function getTokenFromAPI() {
     const tokenUrl = "https://lap-umkm.herokuapp.com/ksi/users/login";
     fetch(tokenUrl)
         .then(response => response.json())
         .then(tokenData => {
-            if (tokenData.token) {
-                userToken = tokenData.token;
+            if (tokenData) {
+                userToken = tokenData;
                 console.log('Token dari API:', userToken);
             }
         })
@@ -54,49 +51,23 @@ export function AlertPost(value) {
     window.location.href = "login.html"
 }
 
-function validateLogin(callback) {
-    // Implement your login validation logic here
-    // Make asynchronous API calls if needed
 
-    // Example: Simulating asynchronous behavior
-    setTimeout(() => {
-        const isValid = true; // Replace with your actual validation logic
-        callback(isValid);
-    }, 1000); // Simulating a delay of 1 second
-}
-
-// response post login
 function ResponsePostLogin(response) {
-    if (response && response.token) {
-        setCookieWithExpireHour('Login', response.token, 2);
+    if (response && response.data) {
+        // Set the 'Login' cookie with a 2-hour expiry
+        setCookieWithExpireHour('login', response.data, 2);
+
+        // Redirect to the dashboard page
         window.location.href = 'https://harisriyoni.github.io/penjahit/public/dashboard.html';
+
+        // Display a success message
         Swal.fire({
             icon: 'success',
             title: 'Masuk Berhasil',
             text: 'Anda telah berhasil masuk!',
         });
     } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Gagal Masuk',
-            text: 'Username atau Kata Sandi tidak valid. Silakan coba lagi.',
-        });
-    }
-}
-
-// handle login
-export function HandleLogin() {
-    // Assuming you have a function to validate the login credentials.
-    const isValidLogin = validateLogin(ResponsePostLogin);
-
-    if (isValidLogin) {
-        // Set npm and nama based on your login logic.
-        username = document.getElementById("username").value;
-
-        // You can also redirect the user to the dashboard here.
-        window.location.href = 'https://harisriyoni.github.io/penjahit/public/dashboard.html';
-    } else {
-        // Handle invalid login
+        // Display an error message
         Swal.fire({
             icon: 'error',
             title: 'Gagal Masuk',
