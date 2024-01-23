7d64f6f55e99616843a98949f71a0ca02d563d4b
Farhan Rizki <farhanrizki101010@gmail.com>
2024-01-24 02:12:32 +0700

    update

--- src/js/user/template/template.js
+++ src/js/user/template/template.js
@@ -1,9 +1,13 @@
-//url register
 export let URLPost = `https://lap-umkm.herokuapp.com/ksi/users/register`
-
-//url login 
 export let URLLogin = `https://lap-umkm.herokuapp.com/ksi/users/login`
-
-//url login 
-
-export let token = 'data';
+export let token = 'token';
+export function getTokenFromCookies(cookieName) {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+        if (name === cookieName) {
+            return value;
+        }
+    }
+    return null;
+}
